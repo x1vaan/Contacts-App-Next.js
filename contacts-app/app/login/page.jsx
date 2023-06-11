bbc88f770b27045b68511fd4fae13169c62c088a
x1vaan <ivanx467@gmail.com>
2023-06-11 01:47:06 -0300

    login page

--- contacts-app/app/login/page.jsx
+++ contacts-app/app/login/page.jsx
@@ -1,10 +1,38 @@
+"use client"
+
 import React from "react";
 import css from './login.module.css'
+import TextField from '@mui/material/TextField';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import Link from 'next/link'
+import { Button } from '@mui/material'
 
 export default function Login(){
+
     return (
         <div className={css.container}>
-            Login
+
+            <div className={css.containerAuth}>
+                <h1>Log in</h1>
+                <HorizontalRuleIcon className={css.line}/>
+                <Link href='/register'><h1 className={css.register}>Register</h1></Link>
+            </div>
+
+            <TextField 
+            variant="outlined" 
+            type="string" 
+            label='Email' 
+            required 
+            className={css.emailInput}
+            />
+            <TextField
+            variant="outlined"
+            type="password"
+            label='Password'
+            required
+            className={css.passwordInput}
+            />
+            <Button variant="contained" className={css.button}>Log in</Button>
         </div>
     )
 }
